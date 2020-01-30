a1ed5e847ed9b7e8fbadecc674b9e3ab207a0706
Katrina_Dierking <kalynndier@gmail.com>
2020-01-30 00:00:22 -0500

    endpoints created

--- router/accountsRouter.js
+++ router/accountsRouter.js
@@ -12,5 +12,87 @@ router.get('/', async (req, res) => {
     };
 });
 
+// -------------------------------//
+// GET ACCOUNTS BY ID -- CREATE//
+// -------------------------------//
+router.get ('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const [account] = await db('account').where('id', id)
+        res.json(account)
+
+    } catch (error) {
+        res.status(500).json 
+        ({
+            success: false,
+            errorMessage: "cannot retrieve account"
+        });
+
+        next(error)
+    } 
+});
+
+
+// -------------------------------//
+// POST ACCOUNTS -- READ          //
+// -------------------------------//
+router.post('/', async (req, res) => {
+    const accountData = {
+        name: req.body.name,
+        budget: req.body.budget
+    }
+    try {
+        const [id] = await db('accounts').insert(accountData);
+        res.status(201).json(await db('accounts').where('id', id))
+    } catch (error) {
+        res.status(500).json 
+        ({
+            success: false,
+            errorMessage: "could not post new account"
+        });
+
+        next(error)
+    }
+});
+
+// -------------------------------//
+// PUT ACCOUNTS -- UPDATE         //
+// -------------------------------//
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params; 
+    try {
+        const account = await db('accounts')
+        .where('id', id)
+        .update(req.body);
+        res.status(200).json({updated: rowsUpdated})
+    } catch (error) {
+        res.status(500).json
+        ({
+            success: false, 
+            errorMessage: "could not update account"
+        });
+    }
+});
+
+// -------------------------------//
+// DELETE ACCOUNTS -- DELETE      //
+// -------------------------------//
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const rowsDeleted = await db('accounts')
+        .where('id', id)
+        .del()
+        res.json ({deletedRecords: rowsDeleted});
+    }catch (error) {
+        res.status(500).json 
+        ({ 
+            success: false, 
+            errorMessage: 'Could not delete account'
+        })
+    }
+});
 
 module.exports = router;
